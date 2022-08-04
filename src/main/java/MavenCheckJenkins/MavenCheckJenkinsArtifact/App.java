7db169b2b2fad79113aaf61dc8c81ce50e31b87a
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-08-04 18:40:16 +0530

    Initial Commit

---
+++ src/main/java/MavenCheckJenkins/MavenCheckJenkinsArtifact/App.java
@@ -0,0 +1,13 @@
+package MavenCheckJenkins.MavenCheckJenkinsArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
